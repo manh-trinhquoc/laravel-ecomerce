8d1bd2af39ada9be5565e51bd54a87960866f527
manhtq <manhtq@unicloud.com.vn>
2022-12-16 09:51:55 +0700

    sp

--- backend/src/route/index.js
+++ backend/src/route/index.js
@@ -5,6 +5,8 @@ import {
 
 import Dashboard from "../views/Dashboard.vue";
 import Login from "../views/Login.vue";
+import RequestPassword from "../views/RequestPassword.vue";
+import ResetPassword from "../views/ResetPassword.vue";
 
 const routes = [{
         path: '/dashboard',
@@ -15,7 +17,23 @@ const routes = [{
         path: '/login',
         name: 'login',
         component: Login
-    }
+    },
+    {
+        path: '/request-password',
+        name: 'requestPassword',
+        component: RequestPassword,
+        meta: {
+            requiresGuest: true
+        }
+    },
+    {
+        path: '/reset-password/:token',
+        name: 'resetPassword',
+        component: ResetPassword,
+        meta: {
+            requiresGuest: true
+        }
+    },
 ]
 
 
